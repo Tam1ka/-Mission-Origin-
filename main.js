839e7684705728cdde088f88346d8a171e3d0912
Fabiane Tami Chimenes <80434869+Tam1ka@users.noreply.github.com>
2021-09-10 12:16:49 -0300

    [Mission: Origin] Finalização da Aula 02

---
+++ main.js
@@ -0,0 +1,32 @@
+/* abre e fecha o menu qndo clicar no icone: hamburgue e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+}
+
+/* qndo clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
+
+/* mudar o header da pag qndo der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header        
+        header.classList.add('scroll')
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
