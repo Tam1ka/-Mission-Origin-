f70bc49886620174ec72faf104ea9cdbf9cfbe40
Fabiane Tami Chimenes <80434869+Tam1ka@users.noreply.github.com>
2021-09-22 10:23:19 -0300

    Add files via upload
    [Mission: Origin] - Aula 03 e 04

--- main.js
+++ main.js
@@ -18,15 +18,54 @@ for(const link of links) {
 }
 
 /* mudar o header da pag qndo der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY >= navHeight) {
-        // scroll é maior que a altura do header        
-        header.classList.add('scroll')
+  if (window.scrollY >= navHeight) {
+  // scroll é maior que a altura do header        
+  header.classList.add('scroll')
+  } else {
+  // menor que a altura do header
+  header.classList.remove('scroll')
+  }
+}
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+   slidesPerView: 1,
+   pagination: {
+     el: '.swiper-pagination'
+   },
+   mousewheel: true,
+   keyboard: true
+})
+
+/* ScrollReveal: mostrar elementos qnd der scroll na pag */
+const scrollReveal = ScrollReveal({
+   origin: 'top',
+   distance: '30px',
+   duration: 700,
+   reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+    { interval: 100 }
+)
+
+/* Botão voltar para o top */
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')        
     } else {
-        // menor que a altura do header
-        header.classList.remove('scroll')
+        backToTopButton.classList.remove('show')
     }
 })
